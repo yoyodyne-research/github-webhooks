608c13c8cf526ed0de630d9bc0dbc0f92eddd24e
Ray Haleblian <rhaleblian@gmail.com>
2023-06-02 10:32:21 -0400

    Use environment secrets

--- test_hello.py
+++ test_hello.py
@@ -1,30 +1,30 @@
-import requests
+# import requests
 import shotgun_api3
 
-base_url = "https://thr3dcgi-staging.shotgunstudio.com" 
-r = requests.get("https://api.github.com/orgs/yoyodyne-research/actions/secrets/SCRIPT_NAME")
-print(r.json())
+# base_url = "https://thr3dcgi-staging.shotgunstudio.com" 
+# r = requests.get("https://api.github.com/orgs/yoyodyne-research/actions/secrets/SCRIPT_NAME")
+# print(r.json())
 #api_key = requests.get("https://api.github.com/orgs/yoyodyne-research/actions/secrets/API_KEY")
 
-# class ShotgunHandler(object):
-#   """
-#   Small class to store the Shotgun API handle.
-#   """
-#   _sg = None
+class ShotgunHandler(object):
+  """
+  Small class to store the Shotgun API handle.
+  """
+  _sg = None
 
-#   @classmethod
-#   def get_conn(cls):
-#       """
-#       :return: Shotgun API handle
-#       """
-#       if not ShotgunHandler._sg:
-#           ShotgunHandler._sg = shotgun_api3.Shotgun(
-#               APP_SETTINGS["SG_SERVER_URL"],
-#               APP_SETTINGS["SG_SCRIPT_NAME"],
-#               APP_SETTINGS["SG_API_KEY"]
-#           )
+  @classmethod
+  def get_conn(cls):
+      """
+      :return: Shotgun API handle
+      """
+      if not ShotgunHandler._sg:
+          ShotgunHandler._sg = shotgun_api3.Shotgun(
+              base_url,
+              os.environ.get("SCRIPT_NAME"),
+              os.environ.get("API_KEY")
+          )
 
-#       return ShotgunHandler._sg
+      return ShotgunHandler._sg
 
 
 def test_hello():
