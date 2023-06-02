c3ca71b83e9008f1db6d30101524830a90d3e08e
Ray Haleblian <ray@haleblian.com>
2023-06-02 10:00:09 -0400

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,25 +1,28 @@
+import requests
 import shotgun_api3
 
+script_name = requests.get("https://api.github.com/orgs/yoyodyne-research/actions/secrets/SCRIPT_NAME")
+api_key = requests.get("https://api.github.com/orgs/yoyodyne-research/actions/secrets/API_KEY")
 
-class ShotgunHandler(object):
-  """
-  Small class to store the Shotgun API handle.
-  """
-  _sg = None
+# class ShotgunHandler(object):
+#   """
+#   Small class to store the Shotgun API handle.
+#   """
+#   _sg = None
 
-  @classmethod
-  def get_conn(cls):
-      """
-      :return: Shotgun API handle
-      """
-      if not ShotgunHandler._sg:
-          ShotgunHandler._sg = shotgun_api3.Shotgun(
-              APP_SETTINGS["SG_SERVER_URL"],
-              APP_SETTINGS["SG_SCRIPT_NAME"],
-              APP_SETTINGS["SG_API_KEY"]
-          )
+#   @classmethod
+#   def get_conn(cls):
+#       """
+#       :return: Shotgun API handle
+#       """
+#       if not ShotgunHandler._sg:
+#           ShotgunHandler._sg = shotgun_api3.Shotgun(
+#               APP_SETTINGS["SG_SERVER_URL"],
+#               APP_SETTINGS["SG_SCRIPT_NAME"],
+#               APP_SETTINGS["SG_API_KEY"]
+#           )
 
-      return ShotgunHandler._sg
+#       return ShotgunHandler._sg
     
     
 def test_hello():
